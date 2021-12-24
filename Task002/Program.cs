3cdad50d187fa5b6d5af1409958de38ea8b98fe4
Alex <alex.kolinson98@mail.ru>
2021-12-24 14:45:54 +0500

    2 task added

---
+++ Task002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число А => ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число B => ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberB == numberA * numberA)
+{
+    Console.Write("Число B является квадратом числа A");
+}
+else
+{
+    Console.Write("Число B не является квадратом числа A");
+}
