2a870674aba10913462fdea10c19d7a1132cbe1b
Alex <alex.kolinson98@mail.ru>
2022-01-12 15:26:59 +0500

    18 task

---
+++ Task018/Program.cs
@@ -0,0 +1,9 @@
+﻿bool[] V1 = { false, true };
+bool[] V2 = { false, true };
+for (int i = 0; i < V1.Length; i++)
+{
+    for (int j = 0; j < V2.Length; j++)
+    {
+        Console.WriteLine($"{V1[i]} + {V2[j]} = {!(V1[i] && V2[j])} {(!V1[i] || !V2[j])}");
+    }
+}
