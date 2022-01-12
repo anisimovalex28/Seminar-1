bb742b8a7d3faf1eaee8bdccac9122daf8445c72
Alex <alex.kolinson98@mail.ru>
2022-01-12 15:27:51 +0500

    003 task

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Console.Write("Введите число А => ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число B => ");
+int numberB = int.Parse(Console.ReadLine());
+Console.Write("Введите число D => ");
+int numberD = int.Parse(Console.ReadLine());
+int max = numberA;
+
+if (numberB > max)
+{
+    int max = numberB
+}
+else 
+{
+    
+}
